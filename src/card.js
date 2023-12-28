443625bda8299a529928f1ca4bf676543937475e
Wasin Kampeera <wasin.kamp@gmail.com>
2023-12-28 20:44:47 +0700

    directly api path in card.js

--- src/card.js
+++ src/card.js
@@ -7,7 +7,7 @@ export default function Card({ room }) {
 
     const handleSubmit = async (rating) => {
         try {
-            const res = await fetch(`${process.env.BASEURL_API}/rate`, {
+            const res = await fetch(`https://restroom-rating.vercel.app/api/rate`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
